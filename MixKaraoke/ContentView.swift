9c472269e47eb8ead8cf5f6672c1e5ad514bfc7c
Ritsuki Kokubo <ritsuki.kokubo@rpaka-farm.jp>
2025-06-12 13:21:01 +0900

    フローレイアウト

--- MixKaraoke/ContentView.swift
+++ MixKaraoke/ContentView.swift
@@ -5,25 +5,40 @@ struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
     @Query private var items: [Item]
     
-    var body: some View {
-        HStack {
-            Text("タイガー")
-                .font(.system(size: 80, weight: .bold))
-                .foregroundStyle(
-                    ProgressGradient.create(progress: 1, opacity: 0.3)
-                )
-            Text("ファイヤー")
-                .font(.system(size: 80, weight: .bold))
-                .foregroundStyle(
-                    ProgressGradient.create(progress: 0.3, opacity: 0.3)
-                )
-            Text("サイバー")
-                .font(.system(size: 80, weight: .bold))
-                .foregroundStyle(
-                    ProgressGradient.create(progress: 0, opacity: 0.3)
-                )
-        }
+    private let lyric = [
+        "人造ファイヤ",
+        "ファイボワイパー",
+        "タイガー",
+        "タイガー",
+        "タタタタタイガー",
+        "チャペアぺカラキナ",
+        "チャペアぺカラキナ",
+        "ミョーホントスケ",
+        "ワイパー",
+        "ファイヤー",
+        "ファイヤー",
+        "虎虎カラキナ",
+        "チャペアぺファーマー",
+        "海人海人ジャスパー",
+        "虎タイガー",
+        "虎タイガー",
+        "人造繊維",
+        "イエッタイガー",
         
+    ]
+    
+    var body: some View {
+        ScrollView {
+            FlowLayout {
+                ForEach(lyric, id: \.self) { lyricWord in
+                    Text(lyricWord)
+                        .font(.system(size: 80, weight: .bold))
+                        .foregroundStyle(
+                            ProgressGradient.create(progress: 0, opacity: 0.3)
+                        )
+                }
+            }.padding(.horizontal, 20)
+        }
     }
 }
 
---
+++ MixKaraoke/FlowLayout.swift
@@ -0,0 +1,52 @@
+import SwiftUI
+
+struct FlowLayout: Layout {
+    let spacing: CGFloat
+    let alignment: VerticalAlignment
+
+    init(spacing: CGFloat = 8, alignment: VerticalAlignment = .center) {
+        self.spacing = spacing
+        self.alignment = alignment
+    }
+
+    func sizeThatFits(proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) -> CGSize {
+        var width: CGFloat = 0
+        var height: CGFloat = 0
+        var rowHeight: CGFloat = 0
+        let maxWidth = proposal.width ?? .infinity
+        var currentRowWidth: CGFloat = 0
+
+        for subview in subviews {
+            let size = subview.sizeThatFits(.unspecified)
+            if currentRowWidth + size.width > maxWidth {
+                height += rowHeight + spacing
+                currentRowWidth = 0
+                rowHeight = 0
+            }
+            currentRowWidth += size.width + spacing
+            rowHeight = max(rowHeight, size.height)
+        }
+        height += rowHeight
+        width = maxWidth
+        return CGSize(width: width, height: height)
+    }
+
+    func placeSubviews(in bounds: CGRect, proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) {
+        var x: CGFloat = bounds.minX
+        var y: CGFloat = bounds.minY
+        var rowHeight: CGFloat = 0
+        // let maxWidth = bounds.width
+
+        for subview in subviews {
+            let size = subview.sizeThatFits(.unspecified)
+            if x + size.width > bounds.maxX {
+                x = bounds.minX
+                y += rowHeight + spacing
+                rowHeight = 0
+            }
+            subview.place(at: CGPoint(x: x, y: y), proposal: ProposedViewSize(width: size.width, height: size.height))
+            x += size.width + spacing
+            rowHeight = max(rowHeight, size.height)
+        }
+    }
+}
