cb1b97b6887da751775b225bebbe8ae561b80876
Ritsuki Kokubo <ritsuki.kokubo@rpaka-farm.jp>
2025-06-13 08:50:41 +0900

    単語ハイライトのアニメーションテスト

--- MixKaraoke/ContentView.swift
+++ MixKaraoke/ContentView.swift
@@ -5,6 +5,10 @@ struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
     @Query private var items: [Item]
     
+    @State private var currentTime: Double = 0
+    @State private var timer: Timer?
+    @State private var isPlaying = false
+    
     private let lyric = [
         "人造ファイヤ",
         "ファイボワイパー",
@@ -28,16 +32,68 @@ struct ContentView: View {
     ]
     
     var body: some View {
-        ScrollView {
-            FlowLayout {
-                ForEach(lyric, id: \.self) { lyricWord in
-                    Text(lyricWord)
-                        .font(.system(size: 80, weight: .bold))
-                        .foregroundStyle(
-                            ProgressGradient.create(progress: 0, opacity: 0.3)
-                        )
-                }
-            }.padding(.horizontal, 20)
+        VStack {
+            ScrollView {
+                FlowLayout {
+                    ForEach(lyric, id: \.self) { lyricWord in
+                        
+                        let progress = currentTime / 3.0
+                        
+                        Text(lyricWord)
+                            .font(.system(size: 80, weight: .bold))
+                            .foregroundStyle(
+                                ProgressGradient.create(progress: progress, opacity: 0.3)
+                            )
+                    }
+                }.padding(.horizontal, 20)
+            }
+        }
+        HStack {
+            Button {playPause()} label: {
+                Image(systemName: isPlaying ? "pause.fill" : "play.fill")
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .frame(width: 80, height: 80)
+                    .background(Color.accentColor)
+                    .clipShape(Circle())
+                    .scaleEffect(isPlaying ? 1.1 : 1.0)
+                    .animation(.easeInOut(duration: 0.2), value: isPlaying)
+            }
+            Button {reset()} label: {
+                Image(systemName: "stop.fill")
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .frame(width: 80, height: 80)
+                    .background(Color.red)
+                    .clipShape(Circle())
+            }
+        }.padding(.top, 5)
+    }
+    
+    private func playPause() {
+        if isPlaying {
+            timer?.invalidate()
+            isPlaying = false
+        } else {
+            startTimer()
+            isPlaying = true
+        }
+    }
+    
+    private func reset() {
+        timer?.invalidate()
+        currentTime = 0
+        isPlaying = false
+    }
+    
+    private func startTimer() {
+        timer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true) { _ in
+            currentTime += 0.01
+            
+//            if let lastTiming = timings.last, currentTime >= lastTiming {
+//                timer?.invalidate()
+//                isPlaying = false
+//            }
         }
     }
 }
